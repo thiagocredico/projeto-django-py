1d0e1ca926958fab6273a06e0f64814d9a644a03
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-10-16 04:26:17 -0300

    feat: create page usuarios and migrations

--- projeto_cad_usuarios/app_cad_usuarios/models.py
+++ projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class Usuarios(models.Model):
+class Usuario(models.Model):
     id_usuario = models.AutoField(primary_key=True)
     nome = models.TextField(max_length=100)
     idade = models.IntegerField()
--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Usuario
 
 # Create your views here.
 
@@ -8,4 +9,13 @@ def home(request):
 
 
 def usuarios(request):
-    pass
+    novo_usuario = Usuario()
+    novo_usuario.nome = request.POST.get("nome")
+    novo_usuario.idade = request.POST.get("idade")
+    novo_usuario.save()
+
+    usuarios = {
+        "usuarios": Usuario.objects.all()
+    }
+
+    return render(request, "usuarios/usuarios.html", usuarios)
