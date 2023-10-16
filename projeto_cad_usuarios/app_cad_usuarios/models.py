ec65324cd4f70dd4a254eeb9d344c8cd6c065c61
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-10-16 03:05:37 -0300

    feat: create Model Usuarios

--- projeto_cad_usuarios/app_cad_usuarios/models.py
+++ projeto_cad_usuarios/app_cad_usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Usuarios(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=100)
+    idade = models.IntegerField()
