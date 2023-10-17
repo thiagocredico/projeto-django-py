ddfc2df94da2cab25812932578dba682a242d895
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-10-17 14:33:23 -0300

    chore: vercel integration on db

--- projeto_cad_usuarios/projeto_cad_usuarios/settings.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = "django-insecure-1!7$)%3l$qct7c6pw(4tiyw360yoq)2_5kajgwe9m1xj(z1ui=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
-
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 # Application definition
 
