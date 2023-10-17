bc608c2848640dd45a6988414ecb19f60124d387
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-10-17 16:27:21 -0300

    chore: change to postgresql db

--- projeto_cad_usuarios/projeto_cad_usuarios/settings.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = "projeto_cad_usuarios.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "usuarios",
+        "USER": "postgres",
+        "PASSWORD": "12345",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
